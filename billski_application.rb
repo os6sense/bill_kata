76ba048e76335f225a96f96a0eeacb8f1c1a86ed
os6sense <leej@sowhatresearch.com>
2015-07-19 22:45:09 +0100

    Added basic project files

--- billski_application.rb
+++ billski_application.rb
@@ -1,15 +1,28 @@
 require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'haml'
+
 require_relative 'lib/bill'
 
 module Billski
   class Application < Sinatra::Base
+    register Sinatra::AssetPack
+
+    assets do
+      js :application, ['/js/*.js']
+      css :application, ['/css/*.css']
+
+      js_compression :jsmin
+      css_compression :sass
+    end
+
     configure do
       set :bill_provider, Bill.new
       enable :logging
     end
 
     get '/' do
-      halt ''
+      haml :index
     end
 
     (Fields::FIELDS << 'bill').each do |name|
